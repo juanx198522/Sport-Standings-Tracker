21465d0d5a287584e7450fbcef2b0d36ae5a9e37
jdelg140 <jacobdelgado140@gmail.com>
2023-05-10 21:34:09 -0400

    edited HTML for soccer and motoring to prepare added information from API

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,25 +20,25 @@ function showSoccer() {
 //     additionalButtonsContainer.classList.toggle("hidden");
 // }
 
-function showMotoring() {
-    var motoringButtons = document.getElementById("motoringButtons");
-    motoringButtons.classList.toggle("hidden");
-    var container = document.getElementById("container");
-    container.classList.toggle("hidden");
-    var sportButtons = document.getElementById("sportButtons");
-    sportButtons.classList.toggle("hidden");
-    var additionalButtonsContainer = document.getElementById("additionalButtonsContainer");
-    additionalButtonsContainer.classList.toggle("hidden");
-}
+// function showMotoring() {
+//     var motoringButtons = document.getElementById("motoringButtons");
+//     motoringButtons.classList.toggle("hidden");
+//     var container = document.getElementById("container");
+//     container.classList.toggle("hidden");
+//     var sportButtons = document.getElementById("sportButtons");
+//     sportButtons.classList.toggle("hidden");
+//     var additionalButtonsContainer = document.getElementById("additionalButtonsContainer");
+//     additionalButtonsContainer.classList.toggle("hidden");
+// }
 
 
 var buttonSoccer = document.getElementById("btnSoccer");
 // var buttonBasketball = document.getElementById("btnBasketball");
 var buttonMotoring = document.getElementById("btnMotoring");
 
-btnSoccer.addEventListener("click", showSoccer);
+// btnSoccer.addEventListener("click", showSoccer);
 // btnBasketball.addEventListener("click", showBasketball);
-btnMotoring.addEventListener("click", showMotoring)
+// btnMotoring.addEventListener("click", showMotoring)
 
 
 
